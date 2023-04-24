70fb9c25d67e55f8e7465977678f4d47bc7e4416
James Gay <jamesgayht@gmail.com>
2023-04-24 19:21:05 +0800

    updated configs

--- server/src/main/java/dateGenie/server/configs/AppConfig.java
+++ server/src/main/java/dateGenie/server/configs/AppConfig.java
@@ -6,12 +6,6 @@ import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -20,20 +14,9 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 
 @Configuration
 public class AppConfig {
-	@Value("${spring.redis.host}")
-	private String redisHost;
-	@Value("${spring.redis.port}")
-	private int redisPort;
-	@Value("${spring.redis.database}")
-	private int redisDatabase;
-
-	@Value("${spring.mongo.url}")
-	private String mongoUrl;
 
 	@Value("${spaces.secret.key}")
 	private String spacesSecretKey;
@@ -54,32 +37,6 @@ public class AppConfig {
 	private String mailUsername;
 	@Value("${spring.mail.password}")
 	private String mailPassword;
-
-
-	@Bean("post")
-	public RedisTemplate<String, String> createRedisTemplate() {
-		final RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
-		config.setDatabase(redisDatabase);
-
-		final JedisClientConfiguration jedisClient = JedisClientConfiguration.builder()
-				.build();
-		final JedisConnectionFactory jedisFac = new JedisConnectionFactory(config, jedisClient);
-		jedisFac.afterPropertiesSet();
-
-		final RedisTemplate<String, String> template = new RedisTemplate<>();
-		template.setConnectionFactory(jedisFac);
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new StringRedisSerializer());
-		template.setHashKeySerializer(new StringRedisSerializer());
-		template.setHashValueSerializer(new StringRedisSerializer());
-		return template;
-	}
-
-	@Bean
-	public MongoTemplate createMongoTemplate() {
-		MongoClient client = MongoClients.create(mongoUrl);
-		return new MongoTemplate(client, "articles");
-	}
 
 	@Bean
 	public AmazonS3 createS3Client() {
